32994a4337b53d57ac3e165344a0ae850d84b94a
dmarinma <dfmarin@indracompany.com>
2022-05-02 21:36:21 -0500

    Primera tarea JS

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -123,7 +123,7 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  return Math.pow(num,exponent);
+  return Math.pow(num,exponent); 
 }
 
 function redondearNumero(num) {
